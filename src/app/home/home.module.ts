1ef8361150362d035b46517c85c30ffdc5f9d3de
Hippopotato22 <ronstedcubar@gmail.com>
2024-04-09 16:40:43 +0800

    filter not working

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage, NotesListComponent, NoteCardComponent, NoteDetailsComponent]
+})
+export class HomePageModule {}
