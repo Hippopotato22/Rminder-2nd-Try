df924a857262bed609d5e294e77a3476f1e3635d
Hippopotato22 <ronstedcubar@gmail.com>
2024-04-10 17:10:07 +0800

    filter not working, alarm design

---
+++ src/app/home/note-alarm/note-alarm.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-alarm',
+  templateUrl: './note-alarm.component.html',
+  styleUrls: ['./note-alarm.component.scss'],
+})
+export class NoteAlarmComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
